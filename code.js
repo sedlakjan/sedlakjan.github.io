590f9e23cc30baf04a260d2ab7dd9d62b63216e9
sedlakjan <sedyjohn@gmail.com>
2022-12-15 12:52:26 +0100

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ let total = document.getElementById('display');
 let outputs = document.getElementById('output');
 let buttons = document.querySelectorAll('.button-21')
 let polozky = [];
-const outputsText = '<p>Gratulujeme k úspešnému nákupu a vytvoreniu finančného cieľa :) Teraz si toto číslo zapíš na papier. Ďalej si budeš musieť na svoj cieľ zarobiť. Klikni na: <a href="https://www.brigady.sk" target="_blank">BRIGADY.SK</a> alebo <a href="https://www.profesia.sk" target="_blank">PROFESIA.SK</a> ,kde si nájdeš brigádu v tvojom okolí. Následne postupuj podľa pokynov na papieri. <br>Veľa štastia :)!</br></p>'; 
+const outputsText = '<p>Gratulujeme k úspešnému nákupu a vytvoreniu finančného cieľa :) Teraz si toto číslo zapíš na papier. Ďalej si budeš musieť na svoj cieľ zarobiť. Klikni na: <a href="https://www.brigada.sk/" target="_blank">BRIGADA.SK</a> alebo <a href="https://www.profesia.sk" target="_blank">PROFESIA.SK</a> ,kde si nájdeš brigádu v tvojom okolí. Následne postupuj podľa pokynov na papieri. <br>Veľa štastia :)!</br></p>'; 
 
 buttons.forEach(button=>{
     button.addEventListener('click',calculate);
