83e72735cbd02c07180944067381f370f2632cc1
sedlakjan <sedyjohn@gmail.com>
2025-05-23 09:27:07 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,36 +1,52 @@
 let total = document.getElementById('display');
 let outputs = document.getElementById('output');
-let buttons = document.querySelectorAll('.button-21')
+let buttons = document.querySelectorAll('.button-21');
 let polozky = [];
-const outputsText = '<p>Gratulujeme k úspešnému nákupu a vytvoreniu finančného cieľa :) Teraz si toto číslo zapíš na papier. Ďalej si budeš musieť na svoj cieľ zarobiť. Klikni na:  <a href="https://www.brigada.sk/" target="_blank">BRIGADA.SK</a> alebo <a href="https://www.profesia.sk" target="_blank">PROFESIA.SK</a> ,kde si nájdeš brigádu v tvojom okolí. Následne postupuj podľa pokynov na papieri. <br>Veľa štastia :)!</br></p>'; 
+let cartBtn   = document.getElementById('vysledok'); // 🚀 nový riadok
 
-buttons.forEach(button=>{
-    button.addEventListener('click',calculate);
 
+const outputsText = '<p>Gratulujeme k úspešnému nákupu a vytvoreniu finančného cieľa :) Teraz si sumu tvojho nákupu zapíš na papier. Ďalej si budeš musieť na svoj cieľ zarobiť. Klikni na: <a href="https://www.brigady.sk" target="_blank">BRIGADY.SK</a> alebo <a href="https://www.profesia.sk" target="_blank">PROFESIA.SK</a>, kde si nájdeš brigádu v tvojom okolí. Následne postupuj podľa pokynov na papieri. <br>Veľa šťastia :)!</br></p>';
+
+buttons.forEach(button => {
+    button.addEventListener('click', calculate);
 });
-function calculate(createarray) {
-    createarray = this.value;
-    polozky.push(createarray); 
-    this.classList.toggle("button-21");
-    this.disabled=true
-    console.log(polozky)
+
+function calculate() {
+    const value = Number(this.value);
+    const index = polozky.indexOf(value);
+
+    if (index === -1) {
+        // Pridaj do zoznamu
+        polozky.push(value);
+        this.classList.add('selected');
+    } else {
+        // Odstráň zo zoznamu
+        polozky.splice(index, 1);
+        this.classList.remove('selected');
+    }
+
+    updateTotal();
 }
+
+function updateTotal() {
+    const suma = polozky.reduce((acc, curr) => acc + curr, 0);
+    total.innerText = suma + ' €';
+    cartBtn.innerHTML = `<i></i> Objednaj (${suma} €)`;
+
+}
+
+
 function summary() {
-    const konverziaArr = polozky.map(str =>{
-        return Number(str);
-}) 
-    let element = 0;
-for (i = 0; i < konverziaArr.length; i++) {
-        element += konverziaArr[i];
-}
-    if (element == 0) {
-        total.innerHTML='<h2><br>Oooops<br>Tvoj košík je prázdny<h2><p>Niečo si kúp a objednaj :)</p>'
+    updateTotal(); 
+    const suma = polozky.reduce((acc, curr) => acc + curr, 0);
+  
+    if (suma === 0) {
+      // Keď ešte nie je v košíku žiadna položka
+      outputs.innerHTML = '<p><strong>Košík je prázdny:</strong> Vyber prosím nejaké produkty.</p>';
+    } else {
+      // Bežný výsledok nákupu
+      outputs.innerHTML = outputsText;
     }
-    else{
-        total.innerText=element + ' €';
-        outputs.innerHTML = outputsText;
-        outputs.classList.add('fade');
-    }
-
-}
-
+    
+    outputs.classList.add('fade');
+  }
